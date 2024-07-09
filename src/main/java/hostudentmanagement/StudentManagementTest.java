1fd9829e735f783aa9674983407433ca77668342
fenilbardoliwala <fenilbardoliwala1212@gmail.com>
2024-07-09 16:54:15 +0530

    Student Management Start

---
+++ src/main/java/hostudentmanagement/StudentManagementTest.java
@@ -0,0 +1,13 @@
+package hostudentmanagement;
+
+import hoadminbasereport.AdminBaseTest;
+import org.testng.annotations.Test;
+
+public class StudentManagementTest extends AdminBaseTest {
+
+    @Test
+    public void StudentManagementListTest()
+    {
+
+    }
+}
