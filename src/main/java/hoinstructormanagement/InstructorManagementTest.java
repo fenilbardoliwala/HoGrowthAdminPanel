df22a19d1c8600ab7be3abce1de86b55fddb74ba
fenilbardoliwala <fenilbardoliwala1212@gmail.com>
2024-07-14 18:08:52 +0530

    Instructor management start

---
+++ src/main/java/hoinstructormanagement/InstructorManagementTest.java
@@ -0,0 +1,96 @@
+package hoinstructormanagement;
+
+import com.aventstack.extentreports.MediaEntityBuilder;
+import hoadminbasereport.AdminBaseTest;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+
+public class InstructorManagementTest extends AdminBaseTest{
+
+    @Test(priority = 1)
+    public void InstructorManagementListTest1() throws IOException, InterruptedException {
+        AdminLogin();
+        Thread.sleep(2000);
+        driver.navigate().refresh();
+        Thread.sleep(2000);
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Student Management']")));
+        } catch (Throwable e) {
+            System.err.println("Error while waiting for the notification to appear: " + e.getMessage());
+        }
+        WebElement StudentManagementMenu = driver.findElement(By.xpath("//span[text()='Student Management']"));
+        StudentManagementMenu.click();
+        try {
+            WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(20));
+            wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tbody")));
+        } catch (Throwable e) {
+            System.err.println("Error while waiting for the notification to appear: " + e.getMessage());
+        }
+        WebElement table = driver.findElement(By.xpath("//tbody"));
+        WebElement PaginationDropdown = driver.findElement(By.xpath("//select[@class='form-control form-control-solid d-inline m-1 px-3 py-2 border rounded-2 w-auto bg-white']"));
+        try {
+            WebDriverWait wait2 = new WebDriverWait(driver, Duration.ofSeconds(20));
+            wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tbody")));
+        } catch (Throwable e) {
+            System.err.println("Error while waiting for the notification to appear: " + e.getMessage());
+        }
+        WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(20)); // Adjust the timeout as needed
+        wait1.until(ExpectedConditions.elementToBeClickable(PaginationDropdown));
+        Select select = new Select(PaginationDropdown);
+        for (WebElement option : select.getOptions()) {
+            select.selectByVisibleText(option.getText());
+            try {
+                WebDriverWait wait3 = new WebDriverWait(driver, Duration.ofSeconds(20));
+                wait3.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tbody/tr")));
+
+            } catch (Throwable e) {
+                System.err.println("Error while waiting for the notification to appear: " + e.getMessage());
+            }
+            List<WebElement> rowsElements = driver.findElements(By.xpath("//tbody/tr"));
+            System.out.println("********Total number of rows " + rowsElements.size() + " in this page********");
+            List<WebElement> columnElements = driver.findElements(By.xpath("//tbody/tr[1]/td"));
+            System.out.println("********Total number of columns " + columnElements.size() + " in this page********");
+            int rowSize = rowsElements.size();
+            int colSize = columnElements.size();
+            for (int i = 1; i <= rowSize; i++)
+            {
+                for (int j = 1; j <= colSize; j++)
+                {
+
+                    System.out.println(driver.findElement(By.xpath("//tbody/tr[" + i + "]/td[" + j + "]")).getText() + " ");
+                }
+                System.out.println();
+            }
+        }
+        System.out.println("\u001B[33m-----TestCases1:-Check student management list page pagination wise data displayed properly-----\u001B[0m");
+        String expected1 = "https://hogrowth.jainam.in/backoffice/apps/student-management/student";
+        String actual1 = driver.getCurrentUrl();
+        test = reports.createTest("Check student management list page pagination wise data displayed properly").assignAuthor("Fenil").assignCategory(getClass().
+                getName()).assignDevice(driver.getClass().getSimpleName()).pass(MediaEntityBuilder.createScreenCaptureFromPath
+                ("./TestcasesScreenshot/screenshots" + takingScreenshot(driver),"Check student management list page pagination wise data displayed properly").build());
+        Assert.assertTrue(true);//pass
+        System.out.println("expected1=" + expected1);
+        System.out.println("actual1=" + actual1);
+
+        if (actual1.equalsIgnoreCase(expected1))
+        {
+            System.out.println("\u001B[32m***Test passed***\u001B[0m");
+        }
+        else
+        {
+            System.out.println("\u001B[31m***Test Failed***\u001B[0m");
+        }
+        Assert.assertEquals("In student management list page data not display properly", expected1, actual1);
+
+    }
+}
